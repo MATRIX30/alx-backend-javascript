dceded1a5fd2034226dc4eee70176723ae8a1031
Matrix30 <Matrixboy30@gmail.com>
2024-04-11 01:02:12 +0100

    lets todo it

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
