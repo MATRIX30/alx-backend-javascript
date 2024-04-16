8b17a4dcdce3a8a9c0c90b17164a335389ec16cb
Matrix30 <Matrixboy30@gmail.com>
2024-04-16 20:15:31 +0100

    finish task 5. Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,10 +3,10 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   // create a new Int8 array from buffer
   const intArray = new Int8Array(buffer);
-
-  if (position >= length) {
+  try {
+    intArray[position] = value;
+    return intArray;
+  } catch (error) {
     throw new Error('Position outside range');
   }
-  intArray[position] = value;
-  return intArray;
 }
