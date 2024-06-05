13eeeba30541b1d5dcea9e69468a75a51ab2b7bf
Matrix30 <Matrixboy30@gmail.com>
2024-06-05 03:01:24 +0100

    finish task 0. Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * This is a program that prints to the STDOUT
  */
-function displayMessage (message) {
+function displayMessage(message) {
   console.log(message);
-};
+}
 module.exports = displayMessage;
